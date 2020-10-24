b3b2b5e50da0ca29c2766c16ce0fa0eec0a2771e
mtd42 <dorville.mathieu@gmail.com>
2020-10-24 16:08:55 +0200

    Update welcome endpoint

--- components/welcome/models.js
+++ components/welcome/models.js
@@ -3,14 +3,14 @@ import moment from 'moment';
 
 const fetchWelcomeData = () => {
     const obj = {
-        message: 'Welcome to schedule-api',
+        message: 'Welcome to the open-api espi-schedule-api',
         version: infos.default.version,
         author: infos.default.author,
-        homepage: infos.default.homepage,
-        date: moment().format('MMMM Do YYYY, h:mm:ss a'),
+        github: infos.default.homepage,
         endpoints: {
             schedules: `${process.env.API_URL}/schedules`,
         },
+        date: moment().format('MMMM Do YYYY, h:mm:ss a'),
     };
     return obj;
 };
